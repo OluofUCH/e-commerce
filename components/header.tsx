83e01404d12ac495d31a5cd745baacc171c7b445
OluofUCH <olubiyidavid441@gmail.com>
2025-07-19 18:50:28 +0100

    update

--- components/header.tsx
+++ components/header.tsx
@@ -6,9 +6,7 @@ const Header = () => {
     <div className="bg-gray-900/90 backdrop-blur-lg shadow-lg border-b border-gray-700/50 flex justify-center fixed w-full top-0 z-50">
 
     <div className="flex justify-between gap-12 items-center px-8 py-4 shadow-xl">
-        <Navlink href="/" name="Home" />
-        <Navlink href="/product" name="Product" />
-        <Navlink href="/dashboard" name="Dashboard" />
+        <Navlink href="/" name="E-coomerce" />
     </div>
     
     </div>
